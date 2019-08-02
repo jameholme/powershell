b35238a52ef0ca04350569e3b89d5d3d54442aad
jameholme <39424878+jameholme@users.noreply.github.com>
2019-08-02 12:05:21 -0400

    Update get_os_from_aduc.ps1

--- get_os_from_aduc.ps1
+++ get_os_from_aduc.ps1
@@ -3,6 +3,6 @@
 #
 
 $ad_path = "OU=,OU=,OU=,DC=,DC=,DC=,DC="
-$computer_list = get-adcomputer -filter * -searchbase $path #uncomment this if you only want so many#|select -first 150
+$computer_list = get-adcomputer -filter * -searchbase $path #uncomment if you only want so many---> #|select -first 150
 $save_path = "C:\Path\Path\file.csv"
 $computer_list.name | %{ Invoke-Command -computername $_ -scriptblock {get-wmiobject win32_operatingsystem | select PSComputername, Name, OSarchitecture} | Export-Csv $save_path -Append -NoClobber -NoTypeInformation}
