36c403b6d5e1504c20251a0694c33f9279012dd1
jameholme <39424878+jameholme@users.noreply.github.com>
2019-11-22 12:53:40 -0500

    Update get_mac_from_aduc.ps1

--- get_mac_from_aduc.ps1
+++ get_mac_from_aduc.ps1
@@ -1,7 +1,7 @@
 #gets computers from aduc and gets their name and mac address information
 
 $path = #change your path. example: "OU=,OU=,OU=,DC=,DC=,DC=,DC="
-$computer_list = get-adcomputer -filter * -searchbase $path #uncomment if you only want so many-> # |select -first 150
+$computer_list = get-adcomputer -filter * -searchbase $path #uncomment if you only want so many-># | select -first 150
 $save_path = #change your save path. example: "C:\Path\File.csv"
 $computer_list.name | %{
     [pscustomobject]@{       
