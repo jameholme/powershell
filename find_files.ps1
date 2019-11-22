28a534d4714ddf7d5a98f9eecef8abb1c6f0c61e
jameholme <39424878+jameholme@users.noreply.github.com>
2019-11-22 12:48:23 -0500

    Update find_files.ps1

--- find_files.ps1
+++ find_files.ps1
@@ -1,5 +1,5 @@
-#finds files, just fill in your information
-#
-#
+#finds files on a share, just fill in your information
 
-get-childitem -recurse \\COMPUTER_or_SERVER_NAME\FOLDER_NAME\ -include *File_Name_You_Are_Looking_For* 
+$share = #change the path to remote share example: '\\COMPUTER_or_SERVER_NAME\FOLDER_NAME\'
+$filename = #the name of the file you are looking for
+get-childitem -recurse $share -include $filename
