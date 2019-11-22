642214b69f38722c7eedcbaa5ef7f7463bc2b2f4
jameholme <39424878+jameholme@users.noreply.github.com>
2019-11-22 12:52:51 -0500

    Update get_mac_from_aduc.ps1

--- get_mac_from_aduc.ps1
+++ get_mac_from_aduc.ps1
@@ -1,20 +1,11 @@
 #gets computers from aduc and gets their name and mac address information
-#
-#
 
-$path = "OU=,OU=,OU=,DC=,DC=,DC=,DC="
-$computer_list = get-adcomputer -filter * -searchbase $path #uncomment if you only want so many---> #|select -first 150
-$save_path = "C:\Path\File.csv
-
+$path = #change your path. example: "OU=,OU=,OU=,DC=,DC=,DC=,DC="
+$computer_list = get-adcomputer -filter * -searchbase $path #uncomment if you only want so many-> # |select -first 150
+$save_path = #change your save path. example: "C:\Path\File.csv"
 $computer_list.name | %{
-
-    [pscustomobject]@{
-        
+    [pscustomobject]@{       
         Name = $_       
         MacAddress = Invoke-Command -computername $_ -scriptblock {(Get-NetAdapter | ?{$_.name -like "Ethernet"}).MacAddress}
-
-    
     } | Export-Csv -Append -NoClobber -NoTypeInformation -Path $save_path
-
 }
-
