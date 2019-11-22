a53e7648268ce0f3eb7c26472b265becf2689b7a
jameholme <39424878+jameholme@users.noreply.github.com>
2019-11-22 12:45:10 -0500

    Create get_group_members.ps1

---
+++ get_group_members.ps1
@@ -0,0 +1,5 @@
+$group =  #change your group name 'group name'
+$path = #change your save path 'C:\Path\File.csv'
+$domain = #change your domain 'domain.local'
+Get-adgroupmember -Identity $group -recursive -server $domain |
+Get-aduser -properties * | select-object displayName, Mail | Export-CSV $path
